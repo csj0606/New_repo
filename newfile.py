504b21f4f6e795e37251205cc6b121ae28a703b8
최성준 <48979141+csj0606@users.noreply.github.com>
2023-03-15 15:55:53 +0900

    committed by comments

--- newfile.py
+++ newfile.py
@@ -1,6 +1,6 @@
 #새로운 깃허브 업로드 연습용 파일
 
-print("Hello World!")
-
+print("Hello everyone! my name is 최성준, and I'm student of HUFS !")
+print("and I'm 21 years old too")
 
 # + 컴퓨터공학부 202203534 최성준 (추가)
