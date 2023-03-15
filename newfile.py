5af1e0264340323090b7e110143e896c3c126b6e
Damrok <49807234+KeyWaveTree@users.noreply.github.com>
2023-03-15 16:48:37 +0900

    not '\n'

--- newfile.py
+++ newfile.py
@@ -1,6 +1,6 @@
 #새로운 깃허브 업로드 연습용 파일
 
-print("Hello everyone! my name is 최성준, and I'm student of HUFS !")
+print("Hello everyone! my name is 최성준, and I'm student of HUFS!", end='')
 print("and I'm 21 years old too")
 
 # + 컴퓨터공학부 202203534 최성준 (추가)
